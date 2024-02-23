1abad8f85f716efe1d6d351560b7b3b15f7792d5
Jimena Leyva <jleyva@bancolombia.com.co>
2024-02-23 09:28:45 -0500

    empty app

--- lib/main.dart
+++ lib/main.dart
@@ -8,13 +8,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(useMaterial3: true),
       title: 'Material App',
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          title: const Text('Creational Design Pattern'),
         ),
         body: const Center(
-          child: Text('Hello World'),
+          child: Text('Welcome to Prototype Pattern'),
         ),
       ),
     );
